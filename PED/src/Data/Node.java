d9510020ddcb10f00093c256f678773b3a5cfdb3
Eliver-Salazar <eliverslzrcmps@gmail.com>
2024-06-30 16:25:46 -0600

    Nodo y Cola

---
+++ PED/src/Data/Node.java
@@ -0,0 +1,46 @@
+package Data;
+
+import javax.xml.crypto.Data;
+
+public class Node {
+    private Data data;
+    private Node next;
+    private Object data;
+
+    public Node() {
+        this.data = null;
+        next = null;
+    }
+    
+    public Node(Data dato) {
+        this.data = dato;
+        next = null;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setDato(Data data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return " " + data +"\n";
+    }
+
+    
+    
+    
+    
+    
+}
---
+++ PED/src/Data/Queue.java
@@ -0,0 +1,132 @@
+package Data;
+
+public class Queue {
+
+    private Node head;
+    private Node last;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
+    
+    //Encolar es lo mismo que agregar
+    public void addNode(Data pData){
+        Node nuevo = new Node(pData);
+        if(isEmpty()){
+            head=last=nuevo;
+        }else{
+            last.setNext(nuevo);
+            last= nuevo;
+        }
+    }
+    
+    public void processNode(){
+        if(isEmpty()){
+            System.out.println("La cola esta vacia");
+        }else{
+            //Aqui recupero la info a retornar antes del if
+            //Nodo aux = cabeza; aux.setSiguiente()=null;
+            if(head==last){
+                
+                head=last=null;
+            }else{
+                head=head.getNext();
+            }
+            //return aux;
+        }
+        
+    }
+    
+    
+    public boolean findNode(int x) {
+        Node aux = head;
+
+        while (aux != null) {
+            if (x == aux.getData().getValue()) {
+                System.out.println("El Valor " + x + " si se encuentra en la pila");
+                return true;
+            }
+            aux = aux.getNext();
+        }
+        System.out.println("El valor " + x + " no se encuentra en la pila");
+        return false;
+
+    }
+    
+    public Node extractNode(int x) {
+        Node r = null;
+        if(x==this.head.getData().getValue()){
+            r=head;
+            this.processNode();
+            r.setNext(null);
+        }else{
+            Node aux = head;
+            while(aux.getNext()!=null){
+                if(aux.getNext().getData().getValue()==x){
+                    r=aux.getNext();
+                    aux.setNext(aux.getNext().getNext());
+                    r.setNext(null);
+                    break;
+                }
+                aux=aux.getNext();
+            }
+        }
+        return r;
+    }
+    
+    public Node getBiggest(){
+        Node r = head;
+        Node aux = this.head;
+        while(aux!=null){
+            if(r.getData().getValue()<aux.getData().getValue()){
+                r=aux;
+            }
+            aux=aux.getNext();
+        }
+        //System.out.println(r+" es el valor mayor");
+        return r;
+    }
+    
+    public void sortQueue(){
+        //se va almacenar los datos de la cola principal
+        Queue temp = new Queue();
+        while(head!=null){
+            temp.addNode(this.getBiggest().getData());
+            this.extractNode(this.getBiggest().getData().getValue());
+        }
+        this.head= temp.getHead();
+        this.last= temp.getLast();   
+        
+    }
+    
+    
+    @Override
+    public String toString() {
+        String r = "";
+        Node aux = head;
+        while(aux != null){
+            r+= aux.toString();
+            aux= aux.getNext();
+        }
+        return r ;
+    }
+}
